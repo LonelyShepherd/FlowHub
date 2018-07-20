8df55a0f6f081bb6d1ae3ed8d84e405840d11aab
Nikola Stojanovikj <nikolastojanovic.hj@hotmail.com>
2018-07-20 19:48:27 +0200

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,47 @@ const path = require('path');
 const dist = path.resolve(__dirname, './dist/');
 
 module.exports = {
-  entry: [
-  	'./src/js/main.js', 
-  	'./src/scss/main.scss'
+	entry: [
+		'./src/js/main.js', 
+		'./src/scss/main.scss'
 	],
-  output: {
-      path: dist,
-      filename: 'js/main.js'
-  },
-  mode: 'production',
-  watch: true,
-  module: {
-	  rules: [
-	    {
-	      test: /\.js$/,
-	      exclude: /(node_modules|bower_components)/,
-	      use: {
-	        loader: 'babel-loader',
-	        options: {
-	          presets: ['babel-preset-env']
-	        }
-	      }
-	    }, 
-	    {
-	    	test: /\.scss$/,
-	    	use: [
-	    		{
-	    			loader: 'file-loader',
-	    			options: {
-	    				name: '[name].css',
-	    				context: dist,
-	    				outputPath: 'css/'
-	    			},
-	    		},
-    			{
+	output: {
+		path: dist,
+		filename: 'js/main.js'
+	},
+	mode: 'production',
+	watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['babel-preset-env']
+					}
+				}
+			}, 
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].css',
+							context: dist,
+							outputPath: 'css/'
+						},
+					},
+					{
 						loader: 'extract-loader'
 					},
 					{
 						loader: 'css-loader',
 						options: {
-                sourceMap: true,
-                minimize: true
-            }
+							sourceMap: true
+						}
 					},
 					{
 						loader: 'postcss-loader'
@@ -54,8 +53,8 @@ module.exports = {
 							sourceMap: true
 						}
 					}
-	    	]
-	    },
-	  ]
+				]
+			},
+		]
 	}
 }
