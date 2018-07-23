83eef22f120a1219d50687683264346d499ecef8
Nikola Stojanovikj <nikolastojanovic.hj@hotmail.com>
2018-07-23 23:10:03 +0200

    Dashboard initial layout

---
+++ FlowHub/Controllers/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using FlowHub.Models;
+
+namespace FlowHub.Controllers
+{
+  public class DashboardController : Controller
+  {
+		private ApplicationDbContext _context;
+
+		public DashboardController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+    // GET: Dashboard
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				if (_context != null)
+				{
+					_context.Dispose();
+					_context = null;
+				}
+			}
+
+			base.Dispose(disposing);
+		}
+	}
+}
