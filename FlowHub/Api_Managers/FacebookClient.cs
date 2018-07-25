a1eb0221505da0ca15bfb3f727290355303e2a25
Darko Filipovski <darkofilipovski0@gmail.com>
2018-07-25 20:17:56 +0200

    Initial working Facebook Http Client

---
+++ FlowHub/Api_Managers/FacebookClient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FlowHub.Api_Managers
+{
+    public interface IFacebookClient
+    {
+        // endpoint e.g "/me"
+        // fields e.g:   "?message=For all Math geniuses :)&link=www.projecteuler.net&access_token=your-access-token"
+        // fields e.g:   "?access_token=your-access-token"
+        // See if you can make it neat
+        Task<string> GetAsync(string endpoint, string fields);
+        Task<string> PostAsync(string endpoint, string fields);
+    }
+
+
+    public class FacebookClient : IFacebookClient, IDisposable
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        private string BaseUri { get; set; }
+
+        public FacebookClient()
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.BaseAddress = new Uri("https://graph.facebook.com/");
+            _client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            //BaseUri = "https://graph.facebook.com/";
+        }
+
+        public async Task<string> GetAsync(string endpoint, string fields)
+        {
+            //Uri uriString = new Uri(String.Format("{0}{1}", BaseUri, endpoint));
+            Uri uriString = new Uri(String.Format("{0}", endpoint));
+
+            string response = await _client.GetStringAsync(uriString);
+
+            return response;
+        }
+
+        public async Task<string> PostAsync(string endpoint, string fields)
+        {
+            //Uri uriString = new Uri(String.Format("{0}{1}{2}", BaseUri, endpoint, fields));
+            Uri uriString = new Uri(String.Format("{0}{1}", endpoint, fields));
+            var values = DictionaryFromFields(fields);
+
+            var content = new FormUrlEncodedContent(values);
+            HttpResponseMessage response = await _client.PostAsync(uriString, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        private Dictionary<string, string> DictionaryFromFields(string fields)
+        {
+            var parts = fields.Remove(0, 1).Split('&');
+            Dictionary<string, string> EscapedValues = parts.ToDictionary(x => x.Split('=')[0], y => y.Split('=')[1]);
+
+            return EscapedValues.ToDictionary(a => a.Key, b => Uri.UnescapeDataString(b.Value));
+        }
+    }
+}
--- FlowHub/Controllers/HomeController.cs
+++ FlowHub/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using FlowHub.Api_Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,37 @@ namespace FlowHub.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        public async Task<ActionResult> PostToFacebook(string msg)
+        {
+            //546349135390552 / feed
+            string response;
+
+            using (FacebookClient _client = new FacebookClient())
+            {
+                var access_token = "EAADlpRefaDYBALnKr5eQ5slpe6ZC4vy0d9aZCRGM1e3ZAZB3WZCWxZA84h6v4q62DngyUQ0o0MDb0s5XvfioQb0i9iuc4pTW7q3QiVAUBNAHGzapbDtjHKnYlFT4mZBOhWjPla4dAOZAVkQBNMuZCfd7Ysy9bkzsTKnTSXHI9yiv8smNM2lrmTJpBq7dE3lFC3Be7JhRJh1KcbhBCpdVbGWcs";
+                 response = await _client.PostAsync("1733163406773721/feed", "?message=" + Uri.EscapeDataString(msg) + "&access_token=" + access_token);
+            }
+
+            return Content(response);
+        }
+
+
+        public ActionResult GetToken()
+        {
+            var app_id = "252497228687414";
+            var baseUri = "https://www.facebook.com/v3.0/";
+            var redirectUri = String.Format("{0}dialog/oauth?client_id={1}&redirect_uri=http://localhost:60272/Home/GettToken", baseUri, app_id);// &state={{st=state123abc,ds=123456789}}
+
+            return Redirect(redirectUri);
+        }
+
+
+        public ActionResult GettToken(string code, string state)
+        {
+            return Content(code + " " + state);
         }
     }
 }
